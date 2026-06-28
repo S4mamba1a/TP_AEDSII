410415447026ec0758538378effb285dec984ea2
Eduardo <ebcotap@gmail.com>
2026-06-28 01:08:42 -0300

    .

--- Codes/busca.c
+++ Codes/busca.c
@@ -1,5 +1,5 @@
 /*
- * Grupo: Eduardo Braga[6568], Victor Laurindo[6580], Victor Costa[6589]
+ * Grupo: Eduardo Braga[6568], Victor Laurindo[6580], Vitor Costa[6589]
  * CCF212 - Algoritmos e Estruturas de Dados II
  * UFV Campus Florestal - 2026/1
  */
